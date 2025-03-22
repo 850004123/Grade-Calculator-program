e68d9188e22697190b3e21afa06a2fea75c0c22e
850004123 <850004123@nzse.ac.nz>
2025-03-23 11:02:24 +1300

    Code update

--- Grade Calculator program/Program.cs
+++ Grade Calculator program/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace Grade_Calculator_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
